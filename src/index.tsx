b2e1a648513fb12a88c87b3ffeafc505731aafeb
Kristófer <kristoferhh@hotmail.com>
2022-05-01 21:57:01 +0000

    Change ReactDOM to createRoot after upgrading to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Store } from "./redux/Store";
-import { Provider } from "react-redux";
-import "../node_modules/@fortawesome/fontawesome-free/js/all";
-import "./i18n/config";
-import "./css/index.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import '../node_modules/@fortawesome/fontawesome-free/js/all'
+import App from './App'
+import './css/index.css'
+import './i18n/config'
+import { Store } from './redux/Store'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(
   <React.StrictMode>
     <Provider store={Store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>
+)
